075fa627d865b23977080cb10e80d81ff521b607
gabrielpedroso <gabrielmacito@gmail.com>
2023-04-12 18:37:41 -0300

    investment-list component

--- src/app/app.material.module.ts
+++ src/app/app.material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
 
+import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button'
 
 const materialModules = [
+    MatTableModule,
     MatButtonModule,
 ];
 
--- src/app/components/investment-list/investment-list.component.ts
+++ src/app/components/investment-list/investment-list.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+export interface Investment {
+  name: string;
+  objective: string;
+  currentBalance: number
+};
+
+const ELEMENT_DATA: Investment[] = [
+  { name: 'INVESTIMENTO 1', objective: 'Minha Aposentadoria', currentBalance: 39321.29 },
+  { name: 'INVESTIMENTO 1', objective: 'Minha Aposentadoria', currentBalance: 39321.29 },
+  { name: 'INVESTIMENTO 1', objective: 'Minha Aposentadoria', currentBalance: 39321.29 },
+  { name: 'INVESTIMENTO 1', objective: 'Minha Aposentadoria', currentBalance: 39321.29 },
+];
+
 @Component({
   selector: 'app-investment-list',
   templateUrl: './investment-list.component.html',
   styleUrls: ['./investment-list.component.scss']
 })
 export class InvestmentListComponent {
-
+  displayedColumns: string[] = ['name', 'objective', 'currentBalance'];
+  dataSource = ELEMENT_DATA;
 }
